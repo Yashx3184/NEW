92792f19d69ad59d8738ea3eb8e7ee68ff1ec997
Yashwanth Gowda R <yashwanth1st2005@gmail.com>
2024-09-22 19:47:53 +0530

    third

--- 1.c
+++ 1.c
@@ -1,8 +1,34 @@
 #include <stdio.h>
-int main() 
+#include <stdlib.h>
+
+char str[100],pat[100],rep[100],ans[100];
+int a,b,c,d,e,flag=0;
+
+void stringmatch() 
 {
-    int n;
-    printf("Enter your age : \n");
-    scanf("%d",&n);
-    printf("Your age is : %d\n",n);
+	while(str[a]!='\0') {
+		if(str[a]!=pat[b]) {
+			str[a]=ans[c];
+			b++,a++;
+		}
+	
+		else
+		printf("Patter string is found and is replaced with the replacement string.");
+		ans[c]=pat[b];
+		printf("The resultant string is : %s",ans);
+			
+		
+	}
 }
+
+int main () {
+printf("Enter the strint:\n");
+gets(str);
+printf("Enter the pattern string:\n");
+gets(pat);
+printf("Enter the replacement string.\n");
+gets(ans);
+stringmatch();
+}
+
+	
